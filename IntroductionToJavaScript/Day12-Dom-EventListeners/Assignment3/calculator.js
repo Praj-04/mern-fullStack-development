dad36781097f6a3bfb19b6555c5426b245019421
Prajna Yogesh <prajna.yogish4@gmail.com>
2022-12-18 23:33:04 -0500

    update project

--- IntroductionToJavaScript/Day12-Dom-EventListeners/Assignment3/calculator.js
+++ IntroductionToJavaScript/Day12-Dom-EventListeners/Assignment3/calculator.js
@@ -1,28 +1,30 @@
-const button = document.getElementsByClassName('box');
-const inputField = document.getElementById('number-input');
-let  infoPara = document.getElementById('info');
+const button = document.getElementsByClassName("box");
+const inputField = document.getElementById("number-input");
+let infoPara = document.getElementById("info");
 
-
-
-for(let i=0;i<button.length;i++){
-    button[i].addEventListener('click', (event) => {
-        // infoPara.style.display='none;'
-let inputText='';
-switch(event.target.innerHTML){
-    case 'C' : break;
-    case '=' : try{
-    inputText = eval(inputField.value);
-    }catch(e){
-inputText='';
-// infoPara.style.display ='block';
-infoPara.innerHTML='Invalid Operation';
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", (event) => {
+    infoPara.style.display = "none;";
+    let inputText = "";
+    switch (event.target.innerHTML) {
+      case "C":
+        break;
+      case "=":
+        try {
+          inputText = eval(inputField.value);
+        } catch (e) {
+          inputText = "";
+          infoPara.style.display = "block";
+          infoPara.innerHTML = "Invalid Operation";
+        }
+        break;
+      case "←":
+        inputText = inputField.value.slice(0, inputField.value.length - 1);
+        break;
+      default:
+        inputText = `${inputField.value}${event.target.innerHTML}`;
     }
-    break;
-    case '←' : inputText =inputField.value.slice(0,inputField.value.length-1)
-    break;
-    default : inputText=`${inputField.value} ${event.target.innerHTML}`
-}
 
-       inputField.value=inputText;
-    });
+    inputField.value = inputText;
+  });
 }
